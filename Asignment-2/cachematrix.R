b898ccc0bc69193a6e11778ac0847b78a6d854b1
Eliana Aliz <elianaaliz16@gmail.com>
2022-01-28 17:37:11 +0100

    Adding code for the functions
    Update the exercises

--- Asignment-2/cachematrix.R
+++ Asignment-2/cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL # Initialize the inverse matrix
+  # Method set
+  set <- function(matrix) {
+     m <<- matrix
+     inv <<- NULL
+  }
+  # Method get
+  get <- function() m
+  
+  # Method to set inverse 
+  setInv <- function(inverse) inv <<- inverse
+  
+  # Method to get inverse
+  getInv <- function() inv
+  
+  # Return a list of methods
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
@@ -12,4 +31,25 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+  ivx <- x$getInv() #Get the inverse of matrix 'x' in this case = 'ivx'
+  
+  if(!is.null(ivx)) { #If its exist
+    message("getting cached data")
+    return(iv) #just return the inverse
+  }
+  # In other case:
+  #Get the inverse matrix from cache
+  m <- x$get()
+  
+  #Calculate the inverse
+  ivx <- solve(m) 
+  
+  ### PLUS ### Looks Good!
+  #We check the result is the identity matrix with this
+  #ivx <- solve(m) %*% m
+  
+  x$setInv(ivx) #Set the inverse
+  
+  ivx #return m
 }
